c897b7a043553c48a2c0ec67c4bebb22ef9f26de
Oscar Garcia <oscaregarcialeon@gmail.com>
2024-01-31 15:47:46 -0300

    Eliminacion de configuracion dark en Nabvar

--- src/components/Navbar/ResponsiveMenu.jsx
+++ src/components/Navbar/ResponsiveMenu.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { FaUserCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
@@ -25,7 +26,7 @@ const ResponsiveMenu = ({ showMenu, setShowMenu }) => {
     <div
       className={`${showMenu ? "left-0" : "-left-[100%]"}
     fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col
-    justify-between bg-white dark:bg-gray-900 dark:text-white
+    justify-between bg-white
     px-8 pb-6 pt-16 text-black transition-all duration-200 
     md:hidden rounded-r-xl shadow-md`}
     >
