e13cdba3b38fe58cdc2109d614a89f266f5d2aa3
Oscar Garcia <oscaregarcialeon@gmail.com>
2024-04-03 12:26:46 -0300

    Ajustes Paleta de Colores en Testimoniales

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -87,15 +87,15 @@ const Testimonial = () => {
               {testimonialData.map(({ id, name, text, img }) => {
                 return (
                   <div key={id} className="my-6">
-                    <div className="flex flex-col justify-center items-center gap-4 text-center shadow-lg p-4 mx-4 rounded-xl bg-primary relative">
+                    <div className="flex flex-col justify-center items-center gap-4 text-center shadow-lg p-4 mx-4 rounded-xl bg-secondary relative">
                       <img
                         src={img}
                         alt=""
                         className="rounded-full block mx-auto"
                       />
                       <h1 className="text-xl font-bold">{name}</h1>
-                      <p className="text-gray-500 text-sm">{text}</p>
-                      <p className="text-black/20 text-9xl font-serif absolute top-0 right-0">
+                      <p className="text-white text-sm">{text}</p>
+                      <p className="text-white text-9xl font-serif absolute top-0 right-0">
                         ,,
                       </p>
                     </div>
